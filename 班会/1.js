3e02e49a547d7fd5fa275af092015efb37ca60e0
hyx <2683443389@qq.com>
2020-05-20 20:13:20 +0800

    这次新增了两个按钮，可以提高或降低被随机选中的概率

--- 班会/1.js
+++ 班会/1.js
@@ -1,41 +1,68 @@
 window.onload = function(){
 let box = this.document.getElementById('box');
 let span = this.document.getElementById('span');
+let but = this.document.getElementById('button');
+let but2 = this.document.getElementById('button2')
 let state = 0;
 let time;
 let gailu = 0;
 let hh;
 let ui = 0;
 let ppp = document.getElementById('ppp');
-let xuehao = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+let xuehao = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
               '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',];
-//span.onclick = function (){
-    //if (state === 0){
-      // clearInterval(time);
-       //time = setInterval(function () {
-       // let hh = Math.round(Math.random() * xuehao.length - 1);
-          //box.innerHTML = xuehao[hh]
-          //console.log(xuehao[hh]);
-         // console.log(hh)
-     // }, 1);
-    //  state = 1
-//}else{
- // state=0;
- // clearInterval(time);
-//}
-//};
+span.onclick = function (){
+    if (state === 0){
+      clearInterval(time);
+        time = setInterval(function () {
+          hh = Math.round(Math.random() * xuehao.length - 1);
+          box.innerHTML = xuehao[hh]
+        //console.log(xuehao[hh]);
+        //console.log(hh)
+       return hh
+     }, 10);
+     state = 1
+}else{
+ state=0;
+ clearInterval(time);
+}
+};
+
+but.onclick = function(){
+  if (xuehao[hh] == undefined){
+    xuehao.splice(hh , 1 , xuehao[xuehao.length - 1])
+  }
+  xuehao.splice(hh , 1)
+  console.log(xuehao);
+  console.log(hh);
+  console.log(xuehao[xuehao.length - 1]);
+  return xuehao;
+};
+
+//唯一的bug是遇到undefined的时候
+but2.onclick = function(){
+  if (xuehao[hh] == undefined){
+    xuehao.splice(hh ,  xuehao[xuehao.length - 1])
+  }
+  xuehao.splice(hh , 0 , hh + 1)
+  console.log(xuehao);
+  console.log(xuehao[xuehao.length - 1]);
+  console.log(hh)
+  return xuehao;
+};
+
   setInterval(function () {
-//  for(let i = 0 ; i < 1600000 ; i++){
+// //  for(let i = 0 ; i < 1600000 ; i++){
     hh = Math.round(Math.random() * xuehao.length - 1);
-     console.log(xuehao[hh]);
+    //  console.log(xuehao[hh]);
      if(hh === -0 || hh === 0 || hh === xuehao.length - 1 || hh === -1){
        gailu ++;
        box.innerHTML = gailu;
        ui = gailu / 1600000
        span.innerHTML = ui;
    }
-   console.log(hh)
-  // }
+  //  console.log(hh)
+//   // }
  }, 1);
 }
 
