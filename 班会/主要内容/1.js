9c4b2e90802964fc513e45c2a121a5be81758668
hyx <2683443389@qq.com>
2020-05-21 21:55:27 +0800

    增加了选题系统，还没做完，就做了一个按钮，并优化了界面

--- 班会/主要内容/1.js
+++ 班会/主要内容/1.js
@@ -2,15 +2,12 @@ window.onload = function(){
 let box = this.document.getElementById('box');
 let span = this.document.getElementById('span');
 let but = this.document.getElementById('button');
-let but2 = this.document.getElementById('button2')
+let but2 = this.document.getElementById('button2');
 let state = 0;
 let time;
-let gailu = 0;
 let hh;
-let ui = 0;
-let ppp = document.getElementById('ppp');
 let xuehao = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-              '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',];
+              '31', '32', '33', '34', '35', '36', '37', '38', '39', '40'];
 span.onclick = function (){
     if (state === 0){
       clearInterval(time);
@@ -50,20 +47,19 @@ but2.onclick = function(){
   console.log(hh)
   return xuehao;
 };
-
-  setInterval(function () {
-// //  for(let i = 0 ; i < 1600000 ; i++){
-    hh = Math.round(Math.random() * xuehao.length - 1);
-    //  console.log(xuehao[hh]);
-     if(hh === -0 || hh === 0 || hh === xuehao.length - 1 || hh === -1){
-       gailu ++;
-       box.innerHTML = gailu;
-       ui = gailu / 1600000
-       span.innerHTML = ui;
-   }
-  //  console.log(hh)
-//   // }
- }, 1);
+//   setInterval(function () {
+// // //  for(let i = 0 ; i < 1600000 ; i++){
+//     hh = Math.round(Math.random() * xuehao.length - 1);
+//     //  console.log(xuehao[hh]);
+//      if(hh === -0 || hh === 0 || hh === xuehao.length - 1 || hh === -1){
+//        gailu ++;
+//        box.innerHTML = gailu;
+//        ui = gailu / 1600000
+//        span.innerHTML = ui;
+//    }
+//   //  console.log(hh)
+// //   // }
+//  }, 1);
 }
 
 
